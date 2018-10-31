97774fd88d83aa3891ded996cd478ac4860a148e
Vandana <vandana@KATAKURI.local>
2018-10-31 12:36:26 +0530

    adding my data to git repo

---
+++ src/main/java/DemoRepoProjectEclipse/DemoRepoProjectEclipse/App.java
@@ -0,0 +1,15 @@
+package DemoRepoProjectEclipse.DemoRepoProjectEclipse;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        System.out.println( "Welcome to the new world" );
+
+    }
+}
