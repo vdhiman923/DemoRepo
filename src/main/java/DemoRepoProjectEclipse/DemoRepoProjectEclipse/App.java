dbea40d71889feab5054b49a910fb65850b1a3c5
Vandana <vandana@KATAKURI.local>
2018-10-31 13:37:33 +0530

    updated and added

--- src/main/java/DemoRepoProjectEclipse/DemoRepoProjectEclipse/App.java
+++ src/main/java/DemoRepoProjectEclipse/DemoRepoProjectEclipse/App.java
@@ -10,6 +10,8 @@ public class App
     {
         System.out.println( "Hello World!" );
         System.out.println( "Welcome to the new world" );
+        System.out.println( "Welcome to the new world 2" );
+
 
     }
 }
---
+++ src/main/java/DemoRepoProjectEclipse/DemoRepoProjectEclipse/post.java
@@ -0,0 +1,11 @@
+package DemoRepoProjectEclipse.DemoRepoProjectEclipse;
+
+public class post {
+	
+public static void main(String arg[])
+
+{
+System.out.print("It is post");
+}
+}
+
